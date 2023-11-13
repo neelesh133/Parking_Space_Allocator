ff5d5cfb6265b044bc488a3cfade8b10b673773c
Neelesh Saxena <saxenaneelo133@gmail.com>
2023-11-13 14:16:50 +0530

    validators added

--- server/Utils/sendEmail2.js
+++ server/Utils/sendEmail2.js
@@ -1,38 +1,34 @@
+const { CourierClient } = require("@trycourier/courier");
 
-const {CourierClient} = require('@trycourier/courier')
+const API_KEY_COURIER = "pk_prod_R87VK9DSNYMHBBMNSNPTB3TH2GCD";
 
-const API_KEY_COURIER = 'pk_prod_R87VK9DSNYMHBBMNSNPTB3TH2GCD'
-
-const courier = CourierClient({authorizationToken:API_KEY_COURIER})
+const courier = CourierClient({ authorizationToken: API_KEY_COURIER });
 
 //send email using courier
-const sendEmail2 = async(mailData)=>{
-    const {subject,receiverMail,html} = mailData;
-    console.log(receiverMail)
-    try{
-        const {requestId} = await courier.send({
-            message:{
-                to:{
-                    email:receiverMail
-                },
-                content:{
-                    title:subject,
-                    body:html,
-                    
-                },
-                routing:{
-                    method:'single',
-                    channels:['email']
-                },
-                
-            }
-        })
-        console.log(requestId)
-        console.log("Mail sent")
-    }catch(err){
-        console.log("Error occured,",err)
-    }
-    
-}
+const sendEmail2 = async (mailData) => {
+  const { subject, receiverMail, html } = mailData;
+  console.log(receiverMail);
+  try {
+    const { requestId } = await courier.send({
+      message: {
+        to: {
+          email: receiverMail,
+        },
+        content: {
+          title: subject,
+          body: html,
+        },
+        routing: {
+          method: "single",
+          channels: ["email"],
+        },
+      },
+    });
+    console.log(requestId);
+    console.log("Mail sent");
+  } catch (err) {
+    console.log("Error occured,", err);
+  }
+};
 
-module.exports = sendEmail2
+module.exports = sendEmail2;
