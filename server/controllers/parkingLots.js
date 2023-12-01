e074917718486fb1c6032a7a6720d2a1d33333ad
Neelesh Saxena <saxenaneelo133@gmail.com>
2023-12-01 23:41:05 +0530

    slots booked by a user route created

--- server/controllers/parkingLots.js
+++ server/controllers/parkingLots.js
@@ -358,3 +358,98 @@ exports.getParkingLots = async (req, res) => {
       return res.status(500).json({ msg: "Something went wrong.." });
     }
   };
+
+// returns the all of the slots booked by a particular user
+exports.getBookedTimeSlots = async (req, res) => {
+    if (!req.userId) {
+      return res.status(401).json({ msg: "Unauthorized" });
+    }
+    console.log("Get Booked time slots for->", req.userId);
+    try {
+      //get all of the slots which are booked by user
+      var bookedTimeSlots = await BookedTimeSlot.find(
+        {
+          booker: req.userId,
+          paid: true,
+        },
+        { carImage: 0 }
+      );
+  
+      //get list of all the lotIDs where user has booked slot at least ones
+      const lotIds = [];
+      for (let slot of bookedTimeSlots) {
+        if (!lotIds.includes(slot.parkingLot)) {
+          lotIds.push(slot.parkingLot);
+        }
+      }
+      console.log(lotIds);
+      //get all the ParkingLot details of those lots which are included in lotIDs
+      var parkingLots = await ParkingLot.find(
+        {
+          _id: {
+            $in: lotIds,
+          },
+        },
+        { lotImages: 0 }
+      );
+      //create a map from lotID to lotDetails for quick access of lockDetails
+      var parkingLotMap = {};
+      for (let lot of parkingLots) {
+        parkingLotMap[lot._id] = {
+          _id: lot._id,
+          name: lot.name,
+          address: lot.address,
+          location: lot.location.coordinates,
+          parkingChargesBike: lot.parkingChargesBike,
+          parkingChargesCar: lot.parkingChargesCar,
+          type: lot.type,
+        };
+      }
+  
+      bookedTimeSlots = bookedTimeSlots.map((timeSlot) => {
+        if (timeSlot.vehicleType == "Bike") {
+          //calculate charges
+          const charges =
+            parkingLotMap[timeSlot.parkingLot].type === "public"
+              ? 0
+              : ((timeSlot.endTime - timeSlot.startTime) / (1000 * 60 * 60)) *
+                parkingLotMap[timeSlot.parkingLot].parkingChargesBike;
+          //pass startTime and endTime as formatted strings
+          //put details of parkingLot instead of just its ID
+          return {
+            ...timeSlot._doc,
+            parkingLot: parkingLotMap[timeSlot.parkingLot],
+            startTime: dayjs(timeSlot.startTime).format("YYYY-MM-DD HH:00"),
+            endTime: dayjs(timeSlot.endTime).format("YYYY-MM-DD HH:00"),
+            charges: charges,
+          };
+        } else {
+          //calculate charges
+          const charges =
+            parkingLotMap[timeSlot.parkingLot].type === "public"
+              ? 0
+              : ((timeSlot.endTime - timeSlot.startTime) / (1000 * 60 * 60)) *
+                parkingLotMap[timeSlot.parkingLot].parkingChargesCar;
+          //pass startTime and endTime as formatted strings
+          //put details of parkingLot instead of just its ID
+          return {
+            ...timeSlot._doc,
+            parkingLot: parkingLotMap[timeSlot.parkingLot],
+            startTime: dayjs(timeSlot.startTime).format("YYYY-MM-DD HH:00"),
+            endTime: dayjs(timeSlot.endTime).format("YYYY-MM-DD HH:00"),
+            charges: charges,
+          };
+        }
+      });
+      console.log("Got Booked slots");
+  
+      return res
+        .status(200)
+        .json({
+          msg: "Booked slots returned for user",
+          bookedTimeSlots: bookedTimeSlots,
+        });
+    } catch (err) {
+      return res.status(500).json({ msg: "Something went wrong.." });
+    }
+  };
