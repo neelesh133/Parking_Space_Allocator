7c85a539ae432d10efa2b4c56f5fa5e21cd6f5bc
Neelesh Saxena <saxenaneelo133@gmail.com>
2023-12-01 23:43:48 +0530

    slot cancelled by a user route created

--- server/controllers/parkingLots.js
+++ server/controllers/parkingLots.js
@@ -453,3 +453,129 @@ exports.getBookedTimeSlots = async (req, res) => {
       return res.status(500).json({ msg: "Something went wrong.." });
     }
   };
+
+//User cancels an already booked slot by himself/herself
+exports.cancelBookedSlot = async (req, res) => {
+    if (!req.userId) {
+      return res.status(401).json({ msg: "Unauthorized" });
+    }
+    try {
+      //get the user requesting to cancel
+      const reqUser = await User.findById(req.userId);
+      console.log(req.body);
+  
+      //if bookedSlot ID is not passed
+      if (!req.body.id) {
+        return res.status(400).json({ msg: "Please pass parameters" });
+      }
+  
+      //get the bookedSlot details
+      const bookedSlot = await BookedTimeSlot.findById(req.body.id, {
+        startTime: 1,
+        endTime: 1,
+        vehicleType: 1,
+        booker: 1,
+        cancellable: 1,
+        parkingLot: 1,
+      });
+  
+      console.log(
+        bookedSlot.startTime,
+        bookedSlot.endTime,
+        bookedSlot.vehicleType,
+        bookedSlot.parkingLot
+      );
+      if (bookedSlot.startTime <= Date.now()) {
+        return res
+          .status(400)
+          .json({ msg: "This slot has started now, you cannot cancel it!" });
+      }
+      //check if the booker of slot same as the one requesting to cancel it
+      if (bookedSlot.booker.toString() !== req.userId.toString()) {
+        return res.status(400).json({ msg: "You haven't booked this slot" });
+      }
+  
+      //a slot can only be cancelled if its cancellable
+      if (!bookedSlot.cancellable) {
+        return res
+          .status(400)
+          .json({ msg: "You cannot cancel this booked slot" });
+      }
+  
+      console.log("Here");
+      //get the parkingLot details
+      const parkingLot = await ParkingLot.findById(bookedSlot.parkingLot, {
+        parkingChargesBike: 1,
+        parkingChargesCar: 1,
+        name: 1,
+        type: 1,
+      });
+  
+      console.log(parkingLot);
+  
+      //send an email to the booker as the confirmation of cancellation of slot
+      const subject = "[Smart Parker] You Cancelled a Booked Slot";
+      var html = "";
+      if (bookedSlot.vehicleType == "Car") {
+        const charges =
+          ((bookedSlot.endTime - bookedSlot.startTime) / (1000 * 60 * 60)) *
+          parkingLot.parkingChargesCar;
+        html = `
+                      Dear ${reqUser.firstName + " " + reqUser.lastName}, 
+                          Your booking for a Car at ${
+                            parkingLot.name
+                          } between ${dayjs(bookedSlot.startTime).format(
+          "DD MMM hh:00 A"
+        )} and ${dayjs(bookedSlot.endTime).format(
+          "DD MMM hh:00 A"
+        )} has been cancelled. 
+                          ${
+                            parkingLot.type === "public"
+                              ? ""
+                              : `The charges for this parking you booked ${charges}, 70% of that will be refunded to your account within 2 days`
+                          }
+              `;
+      } else {
+        const charges =
+          ((bookedSlot.endTime - bookedSlot.startTime) / (1000 * 60 * 60)) *
+          parkingLot.parkingChargesBike;
+        var html = `
+                  Dear ${reqUser.firstName + " " + reqUser.lastName}, 
+                      Your booking for a Bike at ${
+                        parkingLot.name
+                      } between ${dayjs(bookedSlot.startTime).format(
+          "DD MMM hh:00 A"
+        )} and ${dayjs(bookedSlot.endTime).format(
+          "DD MMM hh:00 A"
+        )} has been cancelled. 
+                      ${
+                        parkingLot.type === "public"
+                          ? ""
+                          : `The charges for this parking you booked ${charges}, 70% of that will be refunded to your account within 2 days`
+                      }
+          `;
+      }
+      const receiverMail = reqUser.email;
+      await sendEmail2({ html, subject, receiverMail, name: reqUser.firstName });
+  
+      //update the slot as cancelled and not refunded yet
+      if (parkingLot.type === "public") {
+        await BookedTimeSlot.findByIdAndUpdate(req.body.id, {
+          cancelled: true,
+          cancelledAt: Date.now(),
+          refunded: true,
+        });
+        return;
+      }
+      await BookedTimeSlot.findByIdAndUpdate(req.body.id, {
+        cancelled: true,
+        cancelledAt: Date.now(),
+        refunded: false,
+      });
+      return res
+        .status(200)
+        .json({ msg: "Your Booked Slot Cancelled successfully" });
+    } catch (err) {
+      return res.status(500).json({ msg: "Something went wrong.." });
+    }
+  };
