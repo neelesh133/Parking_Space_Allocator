76fc074e87851b5c35c315f371de1e082081819b
darshbaxi <baxidarsh@gmail.com>
2023-12-01 23:57:49 +0530

    added service worker

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createRoot } from 'react-dom/client';
+import { configureStore } from '@reduxjs/toolkit'
+import authReducer from './state/index'
+import "leaflet/dist/leaflet.css";
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import * as ServiceWorkerRegistration from './serviceWorkerRegistration'
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import {asyncSubsribeUser} from './Utils/subscription'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from './App';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+const store = configureStore({
+    reducer: {
+        auth: authReducer
+    }
+})
+
+const root = createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+        <App />
+    </Provider>
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ServiceWorkerRegistration.register()
+
+asyncSubsribeUser()
